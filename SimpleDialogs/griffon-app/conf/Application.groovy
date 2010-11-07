ed27c3788aacfcee4fbd9faa0461c7ec20053394
James Williams <jwill@jwill-laptop.(none)>
2010-11-06 20:09:09 -0700

    Bump SimpleDialogs app to Griffon 0.9.

--- SimpleDialogs/griffon-app/conf/Application.groovy
+++ SimpleDialogs/griffon-app/conf/Application.groovy
@@ -1,19 +1,12 @@
 application {
-    title='SimpleDialogs'
-    startupGroups = ['SimpleDialogs']
-
-    // Should Griffon exit when no Griffon created frames are showing?
-    autoShutdown = true
-
-    // If you want some non-standard application class, apply it here
-    //frameClass = 'javax.swing.JFrame'
+	title="SimpleDialogs"
+	startupGroups=["SimpleDialogs"]
+	autoShutdown=true
 }
 mvcGroups {
-    // MVC Group for "SimpleDialogs"
-    'SimpleDialogs' {
-        model = 'SimpleDialogsModel'
-        controller = 'SimpleDialogsController'
-        view = 'SimpleDialogsView'
-    }
-
+	SimpleDialogs {
+		model="SimpleDialogsModel"
+		controller="SimpleDialogsController"
+		view="SimpleDialogsView"
+	}
 }
