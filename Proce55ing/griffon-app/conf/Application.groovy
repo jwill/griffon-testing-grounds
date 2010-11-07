dd71941c9024eb63273fb8031bfb0dc445e55fd8
James Williams <jwill@jwill-laptop.(none)>
2010-11-06 20:08:20 -0700

    Bump Proce55ing app to Griffon 0.9.

--- Proce55ing/griffon-app/conf/Application.groovy
+++ Proce55ing/griffon-app/conf/Application.groovy
@@ -1,19 +1,12 @@
 application {
-    title='Proce55ing'
-    startupGroups = ['Proce55ing']
-
-    // Should Griffon exit when no Griffon created frames are showing?
-    autoShutdown = true
-
-    // If you want some non-standard application class, apply it here
-    //frameClass = 'javax.swing.JFrame'
+	title="Proce55ing"
+	startupGroups=["Proce55ing"]
+	autoShutdown=true
 }
 mvcGroups {
-    // MVC Group for "Proce55ing"
-    'Proce55ing' {
-        model = 'Proce55ingModel'
-        view = 'Proce55ingView'
-        controller = 'Proce55ingController'
-    }
-
+	Proce55ing {
+		model="Proce55ingModel"
+		view="Proce55ingView"
+		controller="Proce55ingController"
+	}
 }
